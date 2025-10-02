2274c7b5b7baf5cd23f45cce9ced5489f7c77e6a
Doug Hirlinger <doughirlinger@gmail.com>
2025-10-02 10:38:03 -0400

    create preset function updated to include update for row with same preset number

--- multiplier.php
+++ multiplier.php
@@ -417,19 +417,48 @@ function multiplier_create_preset(WP_REST_Request $request)
         }
     }
 
-    $ok = $wpdb->insert(
-        $table,
-        $row,
-        ['%s', '%d', '%d', '%d', '%s', '%d']
-    );
+    $selected_preset_number = $row["preset_number"];
+    $contains_preset_number = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table WHERE preset_number = %d", $selected_preset_number));
+    $branch = 0;
+    $eval = $contains_preset_number->preset_number == $selected_preset_number;
 
-    if ($ok === false) {
-        return new WP_Error('db_insert_error', 'Could not insert preset', ['status' => 500]);
+    if ($contains_preset_number->preset_number != $selected_preset_number) {
+        $branch = 1;
+        $ok = $wpdb->insert(
+            $table,
+            $row,
+            ['%s', '%d', '%d', '%d', '%s', '%d']
+        );
+
+        if ($ok === false) {
+            return new WP_Error('db_insert_error', 'Could not insert preset', ['status' => 500]);
+        }
+    } else if ($contains_preset_number->preset_number == $selected_preset_number) {
+        $branch = 2;
+        $where = array('preset_number' => $selected_preset_number);
+
+        $ok = $wpdb->update(
+            $table,
+            $row,
+            $where
+        );
+
+        if ($ok === false) {
+            return new WP_Error('db_insert_error', 'Could not insert preset', ['status' => 500]);
+        }
     }
 
     $updated_data =  $wpdb->get_results($wpdb->prepare("SELECT * FROM $table WHERE user_id = %d", $row["user_id"]));
-    return ['success' => true, 'array_id' => (int) $wpdb->insert_id, 'updated_data' => $updated_data];
-    return ['success' => true, 'preset_id' => (int) $wpdb->insert_id];
+
+    foreach ($updated_data as $row) {
+        if (isset($row->params_json)) {
+            $row->params_json = json_decode($row->params_json, true);
+        }
+    }
+
+    return ['cont' => $contains_preset_number->preset_number, 'success' => true, 'array_id' => (int) $wpdb->insert_id, 'updated_data' => $updated_data];
+    // return ['success' => true, 'preset_id' => (int) $wpdb->insert_id];
+    // return ['var' => $selected_preset_number];
 }
 
 function multiplier_get_presets(WP_REST_Request $request)
